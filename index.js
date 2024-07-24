82684f309fa6855c939dbaa1c1d8ee724bd3104c
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-07-24 16:57:23 +0700

    create project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+
+// app.set("views", `${__dirname}/views`);
+// app.set("view engine", "pug");
+
+app.get("/",(req, res)=>{
+  res.send("trang chủ")
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
