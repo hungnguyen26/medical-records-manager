5198be702d3c8a35843fad1c7fdf3a15ec95345f
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-07-24 23:33:06 +0700

    connect db

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const database = require("./config/database");
 
 const app = express();
 const port = 3000;
 
+dotenv.config();
+database.connect();
 
-// app.set("views", `${__dirname}/views`);
-// app.set("view engine", "pug");
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
 
 app.get("/",(req, res)=>{
   res.send("trang chủ")
