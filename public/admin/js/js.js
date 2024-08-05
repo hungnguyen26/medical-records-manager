ab3bc0094316608cb7725c5a02bd4159c1c1c406
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-08-05 19:22:36 +0700

    hiển thị thời gian thực

--- public/admin/js/js.js
+++ public/admin/js/js.js
@@ -32,15 +32,12 @@ if (short_alert) {
 }
 //end thông báo
 
-
-// prview upload img 
+// prview upload img
 const uploadImg = document.querySelector("[upload-img]");
 if (uploadImg) {
   const uploadImgInput = document.querySelector("[upload-img-input]");
   const uploadImgpreview = document.querySelector("[upload-img-preview]");
   const deleteImg = document.querySelector(".delete-img");
-
-
 
   uploadImgInput.addEventListener("change", (event) => {
     const file = event.target.files[0];
@@ -49,10 +46,24 @@ if (uploadImg) {
     }
   });
 
-    deleteImg.addEventListener("click", () => {
-      uploadImgInput.value = "";
-      uploadImgpreview.src = "";
+  deleteImg.addEventListener("click", () => {
+    uploadImgInput.value = "";
+    uploadImgpreview.src = "";
   });
- 
 }
-// end prview upload img 
+// end prview upload img
+
+// hiển thị thời gian thực
+const updateTime = () => {
+  const clockTime = document.getElementById("clock-time-item");
+
+  const now = new Date();
+  const hours = String(now.getHours());
+  const getMinutes = String(now.getMinutes());
+  const seconds = String(now.getSeconds());
+  // console.log(hours);
+  clockTime.innerHTML = `${hours} : ${getMinutes} : ${seconds}`;
+  setTimeout(updateTime,1000);
+};
+updateTime();
+//end hiển thị thời gian thực
