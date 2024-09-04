d31ad8f44777cb3bbd6f116ca47fb0352283e06d
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-09-04 18:00:54 +0700

    thêm thuốc

--- controllers/admin/medicines.controller.js
+++ controllers/admin/medicines.controller.js
@@ -1,6 +1,9 @@
 const Medicine = require("../../models/medicines.model");
 const searchHelper = require("../../helpers/search");
 
+const systemConfig = require("../../config/system");
+
+
 // [GET] admin/medicines
 module.exports.index = async (req, res) => {
   const find = {};
@@ -32,7 +35,24 @@ module.exports.create = async (req, res) => {
 
 // [POST] admin/medicines
 module.exports.createPost = async (req, res) => {
-  console.log(req.body);
-  
+  // console.log(req.body);
+  const tenThuocTonTai = await Medicine.findOne({
+    name: req.body.name
+  })
+  if(tenThuocTonTai){
+    req.flash("thatbai", " Tên thuốc đã tồn tại.");
+    res.redirect("back");
+  }else{
+      const record = new Medicine(req.body);
+      await record.save();
+      
+      req.flash("thanhcong", " Thêm thuốc thành công.");
+      res.redirect(`${systemConfig.prefixAdmin}/medicines`);
+  }
+};
+
+// [PATCH] admin/medicines/edit/:id
+module.exports.edit = async (req, res) => {
+  console.log(req.params.id); 
   res.send("Ok")
 };
