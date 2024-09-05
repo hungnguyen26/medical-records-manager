359773d2782f7f384ad199300b377c956eec9cd7
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-09-05 14:01:52 +0700

    Update medicines.validate.js

--- validates/admin/medicines.validate.js
+++ validates/admin/medicines.validate.js
@@ -4,5 +4,20 @@ module.exports.createPost = (req, res, next)=>{
         res.redirect("back");
         return;
     }
+    if(!req.body.quantity){
+        req.flash("thatbai","Vui lòng nhập số lượng thuốc !! ");
+        res.redirect("back");
+        return;
+    }
+    if(!req.body.price){
+        req.flash("thatbai","Vui lòng nhập giá thuốc !! ");
+        res.redirect("back");
+        return;
+    }
+    if(!req.body.usage){
+        req.flash("thatbai","Vui lòng chọn cách dùng !! ");
+        res.redirect("back");
+        return;
+    }
     next();
 }
