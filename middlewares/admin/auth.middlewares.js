5927c599aa072a6bbb28d46a6b58396e8353a28c
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-09-07 16:25:22 +0700

    Update

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -16,7 +16,7 @@ module.exports.requireAuth = async (req, res, next) => {
 
   const role = await Role.findOne({
     _id: user.Role_id,
-  }).select("title quyen");
+  }).select("title quyen description");
 
   res.locals.User = user; // tạo biến toàn cục tên user, tất cả file pug đều dùng được user
   res.locals.Role = role;
