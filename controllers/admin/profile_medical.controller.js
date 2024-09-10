4d77f3ed4e79ae1401f1049c5786777790b24c8b
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-09-10 20:11:52 +0700

    UP

--- controllers/admin/profile_medical.controller.js
+++ controllers/admin/profile_medical.controller.js
@@ -1,6 +1,8 @@
 const User = require("../../models/users.model");
 const searchHelper = require("../../helpers/search");
 
+const systemConfig = require("../../config/system");
+
 // [GET] admin/profile-medical
 module.exports.index = async (req, res) => {
   const find = { deleted: false };
@@ -13,10 +15,41 @@ module.exports.index = async (req, res) => {
   // end search
 
   const users = await User.find(find);
-
+  // console.log(users);
+  
   res.render("admin/pages/administrative-staff/profile-medical/index.pug", {
     pageTitle: "Hồ sơ bệnh án",
     users: users,
     keyword: objectSearch.keyword
   });
+};
+
+
+// [GET] admin/profile-medical/create-patient
+module.exports.createPatient = async (req, res) => {
+  
+  res.render("admin/pages/administrative-staff/profile-medical/createPatient.pug", {
+    pageTitle: "Tạo hồ sơ bệnh nhân"
+  });
+};
+
+// [POST] admin/profile-medical/create-patient
+module.exports.createPatientPost = async (req, res) => {
+  try {
+    if(req.body.dateOfBirth){
+      req.body.dateOfBirth = new Date(req.body.dateOfBirth);
+    }
+    const newPatient = new User(req.body);
+    await newPatient.save();
+
+    req.flash("thanhcong", " Thêm bệnh nhân mới.");
+    res.redirect(`${systemConfig.prefixAdmin}/profile-medical`);
+
+  } catch (error) {
+
+    console.log(error);
+    req.flash("thatbai", "Có lỗi xảy ra khi thêm bệnh nhân.");
+    res.redirect(`${systemConfig.prefixAdmin}/profile-medical`);
+  }
+  
 };
--- routers/admin/profile-medical.router.js
+++ routers/admin/profile-medical.router.js
@@ -5,4 +5,8 @@ const controller = require("../../controllers/admin/profile_medical.controller")
 
 router.get("/", controller.index);
 
+router.get("/create-patient", controller.createPatient);
+
+router.post("/create-patient", controller.createPatientPost);
+
 module.exports = router;
