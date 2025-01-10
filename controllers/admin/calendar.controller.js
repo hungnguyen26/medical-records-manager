e3d1466826d35ba04dffa23d6896509312c05b8a
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-01-11 01:07:47 +0700

    UP

--- controllers/admin/calendar.controller.js
+++ controllers/admin/calendar.controller.js
@@ -4,3 +4,18 @@ module.exports.index = async (req, res) => {
     pageTitle: "Lịch biểu",
   });
 };
+
+
+module.exports.create = async (req, res) => {
+  const date = req.query;
+  res.render("admin/pages/calendars/create.pug", {
+    pageTitle: "Tạo cuộc họp",
+    date:date
+  });
+};
+
+module.exports.createPost = async (req, res) => {
+  console.log(req.body);
+  
+};
+
--- routers/admin/calendars.router.js
+++ routers/admin/calendars.router.js
@@ -5,6 +5,11 @@ const controller = require("../../controllers/admin/calendar.controller");
 
 router.get("/", controller.index);
 
+router.get("/meeting/create", controller.create);
+
+router.post("/meeting/create", controller.createPost);
+
+
 
 
 module.exports = router;
