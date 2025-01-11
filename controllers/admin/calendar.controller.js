2e3fdc2369a99103f9a437732048bfcd1dd0d207
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-01-12 00:04:02 +0700

    Up

--- controllers/admin/calendar.controller.js
+++ controllers/admin/calendar.controller.js
@@ -1,3 +1,6 @@
+const Role = require("../../models/roles.model");
+const Account = require("../../models/accounts.model");
+
 // [GET] admin/home
 module.exports.index = async (req, res) => {
   res.render("admin/pages/calendars/index.pug", {
@@ -5,17 +8,26 @@ module.exports.index = async (req, res) => {
   });
 };
 
-
 module.exports.create = async (req, res) => {
   const date = req.query;
+  const roleDoctor = await Role.findOne({ quyen: "doctor" });
+  // console.log(roleDoctor);
+
+  const doctors = await Account.find({
+    Role_id: roleDoctor.id,
+    deleted: false,
+    status: "active",
+    _id: { $ne: res.locals.User.id },
+  });
+  // console.log(doctors);
+
   res.render("admin/pages/calendars/create.pug", {
     pageTitle: "Tạo cuộc họp",
-    date:date
+    date,
+    doctors,
   });
 };
 
 module.exports.createPost = async (req, res) => {
-  console.log(req.body);
-  
+ res.send("ok");
 };
-
